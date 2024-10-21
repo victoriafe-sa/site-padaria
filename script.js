a769b7bca777abd0c295d76f49d6735f34a138c4
Victoria Ferreira <victoria.senac.13@gmail.com>
2024-10-21 19:01:15 -0300

    mudanças

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+let count = 0;
 function alertCarrinho(){
-    alert("Produto adcionado no carrinho com sucesso!");
-
+    document.getElementById("sound").play();
+   
+    count++;
+    document.getElementById("clickCount").innerHTML = count;
 }
 function alertForm() {
-    alert("Formulário enviado com sucesso!")
+    alert("Formulário enviado com sucesso!");
 
 }
 
@@ -11,14 +14,17 @@ function alertForm() {
 function changeTema() {
     var cabeca = document.getElementById("cabeca");
     var corpo = document.getElementById("corpo");
+    var pe = document.getElementById("pe");
     var corCabeca = "#dda37a"; // cor de referência
 
     // Verifica se a cor de fundo é igual à cor de referência
     if (cabeca.style.backgroundColor === corCabeca || (getComputedStyle(cabeca).backgroundColor === "rgb(221, 163, 122)")) { // Verifica em formato rgb
         cabeca.style.backgroundColor = 'white';
         corpo.style.backgroundColor = corCabeca;
+        pe.style.backgroundColor = 'white';
     } else {
         corpo.style.backgroundColor = 'white';
         cabeca.style.backgroundColor = corCabeca;
+        pe.style.backgroundColor = corCabeca;
     }
 }
